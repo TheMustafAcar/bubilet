5c579ed4eaf98fb8c834cf42b33823dbce3dd5a1
TheMustafAcar <ma485376@gmail.com>
2023-11-21 01:28:18 +0300

    commit

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (isset($_SESSION["id"])) {
     <link rel="stylesheet" href="index.css"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>BUBilet (En Uygun Otobüs ve Uçak Bileti) <?php echo $name ?></title>
+    <title>BUBilet (En Uygun Otobüs ve Uçak Bileti)</title>
     
 </head>
 <body>
